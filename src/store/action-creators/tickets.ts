15b4a90ae6ecfd7bb0717402feab000f0bd0fe72
bektemirova <jibek260886@gmail.com>
2023-02-22 11:58:40 +0300

    little refactor

--- src/store/action-creators/tickets.ts
+++ src/store/action-creators/tickets.ts
@@ -1,9 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { TicketsActionTypes } from '../../types/ticketsType';
+import { TicketsObjectType, TicketsActionTypes } from '../../types/ticketsType';
 import { AnyAction } from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
 
 const API_BASE = 'https://aviasales-test-api.kata.academy/';
+
+export const setId = (id: string) => ({
+  type: TicketsActionTypes.SEARCH_ID,
+  payload: id,
+});
+export const startLoading = (loading: boolean) => ({
+  type: TicketsActionTypes.TICKETS_LOADING,
+  payload: loading,
+});
+
+export const cathError = (error: boolean | string) => ({
+  type: TicketsActionTypes.TICKETS_ERROR,
+  payload: error,
+});
+
+export const getTicketsData = (data: TicketsObjectType) => ({
+  type: TicketsActionTypes.TICKETS_SUCCESS,
+  payload: data,
+});
 
 export const getIdSearch = () => {
   return async (dispatch: ThunkDispatch<Record<string, unknown>, Record<string, unknown>, AnyAction>) => {
@@ -11,19 +30,16 @@ export const getIdSearch = () => {
       fetch(`${API_BASE}search`)
         .then((data) => data.json())
         .then(({ searchId }) => {
-          dispatch({ type: TicketsActionTypes.SEARCH_ID, payload: searchId });
+          dispatch(setId(searchId));
 
           return searchId;
         })
         .then((searchId) => {
-          dispatch({ type: TicketsActionTypes.TICKETS_LOADING, payload: true });
+          //dispatch(startLoading(true));
           dispatch(fetchTickets(searchId));
         });
     } catch (e) {
-      dispatch({
-        type: TicketsActionTypes.TICKETS_ERROR,
-        payload: true,
-      });
+      dispatch(cathError(true));
     }
   };
 };
@@ -39,25 +55,23 @@ const fetchTickets = (searchId: string) => {
           return data.json();
         })
         .then(({ tickets, stop }) => {
-          dispatch({ type: TicketsActionTypes.TICKETS_LOADING, payload: true });
+          dispatch(startLoading(true));
 
-          dispatch({ type: TicketsActionTypes.TICKETS_SUCCESS, payload: tickets });
+          dispatch(getTicketsData(tickets));
           if (!stop) dispatch(fetchTickets(searchId));
-          if (stop) {
-            dispatch({ type: TicketsActionTypes.TICKETS_LOADING, payload: false });
-          }
+          if (stop) dispatch(startLoading(false));
         })
         .catch((error) => {
           if (error.status === 500) {
             dispatch(fetchTickets(searchId));
           } else {
-            dispatch({ type: TicketsActionTypes.TICKETS_ERROR, payload: true });
-            dispatch({ type: TicketsActionTypes.TICKETS_LOADING, payload: false });
+            dispatch(cathError(true));
+            dispatch(startLoading(false));
           }
         });
     } catch (error: any) {
-      dispatch({ type: TicketsActionTypes.TICKETS_ERROR, payload: true });
-      dispatch({ type: TicketsActionTypes.TICKETS_LOADING, payload: false });
+      dispatch(cathError(error?.message));
+      dispatch(startLoading(false));
     }
   };
 };
